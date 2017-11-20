8810b18fd785abd5ad9662c70dcb972504b2dc5e
MYKYTA ZAMIATIN <4406015@gmail.com>
2017-11-20 18:35:03 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
 //define functions here
-$('h1').on("getIt", function(){
-    //action you want taken
-});
+function getIt() {
+  $('p').on('click', function() {
+    alert('hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    // 'g' corresponds to 71
+    if (e.which === 78) {
+      alert("You pressed 'N'!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
+}
 
 
 $(document).ready(function(){
